fa83e47ff1905caa474a3c7ade2e59f50b7fb6e9
Carl <jancarlpeters@gmail.com>
2021-03-11 19:43:33 +0100

    initial commit

---
+++ Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + Input.GetAxis("Mouse X") * speed, 0);
+    }
+}
