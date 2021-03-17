f0c4e4e776a6aeb0855f3ad39da90442127342c9
Carl <jancarlpeters@gmail.com>
2021-03-17 21:03:33 +0100

    Demo V2
    Demo with alternate scale option

---
+++ Assets/Scripts/DemoSceneSwitch.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DemoSceneSwitch : MonoBehaviour
+{
+    Scene currentScene;
+    private void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Scene Switch"))
+        {
+            if (currentScene.buildIndex == 0)
+            {
+                SceneManager.LoadScene(1);
+            } else if (currentScene.buildIndex == 1)
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
