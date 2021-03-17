d64ec590a14f35b966764e1a994caaad4ecc0391
Carl <jancarlpeters@gmail.com>
2021-03-18 00:53:57 +0100

    added art test to demo

--- Assets/Scripts/DemoSceneSwitch.cs
+++ Assets/Scripts/DemoSceneSwitch.cs
@@ -18,7 +18,12 @@ public class DemoSceneSwitch : MonoBehaviour
             if (currentScene.buildIndex == 0)
             {
                 SceneManager.LoadScene(1);
-            } else if (currentScene.buildIndex == 1)
+            }
+            else if (currentScene.buildIndex == 1)
+            {
+                SceneManager.LoadScene(2);
+            }
+            else if (currentScene.buildIndex == 2)
             {
                 SceneManager.LoadScene(0);
             }
